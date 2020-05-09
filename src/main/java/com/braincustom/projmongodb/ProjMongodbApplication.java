955d2fd6be9efec16cbbb56f415849efd2d57750
hvalmer <hvalmer@gmail.com>
2020-05-09 18:17:06 -0300

    Projeto criado

---
+++ src/main/java/com/braincustom/projmongodb/ProjMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.braincustom.projmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjMongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/braincustom/projmongodb/ProjMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.braincustom.projmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
