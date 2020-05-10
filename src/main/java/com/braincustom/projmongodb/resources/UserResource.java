e4ca052f1d6f05a9a6563fb11ba18811091cbb58
hvalmer <hvalmer@gmail.com>
2020-05-10 20:00:32 -0300

    Usando padrão DTO para retornar usuários

--- src/main/java/com/braincustom/projmongodb/resources/UserResource.java
+++ src/main/java/com/braincustom/projmongodb/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.braincustom.projmongodb.resources;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.braincustom.projmongodb.domain.User;
+import com.braincustom.projmongodb.dto.UserDTO;
 import com.braincustom.projmongodb.services.UserService;
 
 @RestController
@@ -22,8 +24,10 @@ public class UserResource {
 	
 	//método que retorna uma lista de usuários
 	@RequestMapping(method=RequestMethod.GET)
-	public ResponseEntity<List<User>> findAll(){
+	public ResponseEntity<List<UserDTO>> findAll(){
 		List<User> list = service.findAll();
-		return ResponseEntity.ok().body(list);
+		//nesta linha, consigo converter da minha lista original para um DTO
+		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 	}
 }
