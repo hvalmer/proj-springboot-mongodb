e6f9e984c0ac1a098f7dfbcfa720af0c3bd2cdcf
hvalmer <hvalmer@gmail.com>
2020-05-10 23:24:29 -0300

    Inserção de usuário com POST

--- src/main/java/com/braincustom/projmongodb/resources/UserResource.java
+++ src/main/java/com/braincustom/projmongodb/resources/UserResource.java
@@ -1,14 +1,17 @@
 package com.braincustom.projmongodb.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.braincustom.projmongodb.domain.User;
 import com.braincustom.projmongodb.dto.UserDTO;
@@ -18,25 +21,32 @@ import com.braincustom.projmongodb.services.UserService;
 @RequestMapping(value = "/users")
 public class UserResource {
 
-	
-	//injetando o serviço
+	// injetando o serviço
 	@Autowired
 	private UserService service;
-	
-	//método que retorna uma lista de usuários
-	@RequestMapping(method=RequestMethod.GET)
-	public ResponseEntity<List<UserDTO>> findAll(){
+
+	// método que retorna uma lista de usuários
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<UserDTO>> findAll() {
 		List<User> list = service.findAll();
-		//nesta linha, consigo converter da minha lista original para um DTO
+		// nesta linha, consigo converter da minha lista original para um DTO
 		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDto);
 	}
-	
-	//método que retorna um usuário por id
-		@RequestMapping(value="/{id}", method=RequestMethod.GET)
-		public ResponseEntity<UserDTO> findById(@PathVariable String id){
-			User obj = service.findById(id);
-			return ResponseEntity.ok().body(new UserDTO(obj));
-		}
-	
+
+	// método que retorna um usuário por id
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(new UserDTO(obj));
+	}
+
+	// método que insere um novo usuário
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody UserDTO objDto) {
+		User obj = service.fromDTO(objDto);
+		obj = service.insert(obj);
+		URI uri= ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
 }
