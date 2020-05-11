1cd08bf0bc0dc662452bfce5c3c56e44d2fc9b41
hvalmer <hvalmer@gmail.com>
2020-05-11 15:28:01 -0300

    Criando entity Post com User aninhado

---
+++ src/main/java/com/braincustom/projmongodb/repository/PostRepository.java
@@ -0,0 +1,12 @@
+package com.braincustom.projmongodb.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.braincustom.projmongodb.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+
+	
+}
