6224abed4423fb3512408b04c2332893254290cb
hvalmer <hvalmer@gmail.com>
2020-05-11 16:53:07 -0300

    Obtendo um post por id

---
+++ src/main/java/com/braincustom/projmongodb/resources/PostResource.java
@@ -0,0 +1,27 @@
+package com.braincustom.projmongodb.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.braincustom.projmongodb.domain.Post;
+import com.braincustom.projmongodb.services.PostService;
+
+@RestController
+@RequestMapping(value = "/posts")
+public class PostResource {
+
+	// injetando o serviço
+	@Autowired
+	private PostService service;
+
+	// método que retorna um usuário por id
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<Post> findById(@PathVariable String id) {
+		Post obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+}
---
+++ src/main/java/com/braincustom/projmongodb/services/PostService.java
@@ -0,0 +1,22 @@
+package com.braincustom.projmongodb.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.braincustom.projmongodb.domain.Post;
+import com.braincustom.projmongodb.repository.PostRepository;
+import com.braincustom.projmongodb.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired //mecanismo de injeção automática do spring
+	private PostRepository repo; 
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado!"));
+	}
+}
